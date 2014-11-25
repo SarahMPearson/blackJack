880f1a8435af26712e94d197e84ecda117f13d74
Sarah Pearson <sarahp@me.com>
2014-11-24 21:30:47 -0600

    name shows up next to message

--- client/views/rooms/rooms_list.js
+++ client/views/rooms/rooms_list.js
@@ -5,12 +5,17 @@
     .controller('RoomsListCtrl', ['$rootScope', '$scope', '$state', function($rootScope, $scope, $state){
 
       $scope.chat = function(msg){
+        msg = $scope.rootuser.username + ': ' + msg;
+        //debugger;
         socket.emit('globalChat', msg);
+        //msg = '';
       };
 
       socket.on('bGlobalChat', function(data){
         $('#messages').append('<div>' + data + '</div>');
+          //$('#messages').val('');
       });
+
 
     }]);
 })();
